d6745949472744f5d7d76cc04aece1939bf2f08d
Emmanuel700-spec <emmanuelkipkirui73@gmail.com>
2024-10-21 19:44:05 +0300

    first commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -12,7 +12,7 @@ function Signup() {
         e.preventDefault();
 
         try {
-            const response = await fetch('http://localhost:5000/api/signup', {
+            const response = await fetch('https://note-taking-backend-project-a5ar.onrender.com/api/signup', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
